42425630b8d0ae31caf2d10140bafcd43fa28af7
jvras58 <jvras@cin.ufpe.br>
2025-12-04 11:41:37 -0300

    refactor: Remove comentários desnecessários nos carregadores de dados e caminhos

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -4,8 +4,6 @@ import sys
 
 def get_assets_path():
     if getattr(sys, "frozen", False):
-        # Estamos no executável PyInstaller
         return os.path.join(sys._MEIPASS, "assets")
     else:
-        # Desenvolvimento
         return "src/assets"
